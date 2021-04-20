03af2b3535a9dddc472ee35c82b5772f4c532be9
Plan49 <anthonyplanisek1@gmail.com>
2021-04-20 15:26:13 -0400

    added the timer

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var start = document.getElementById("start")
 var quiz = document.getElementById("quiz")
 var question = document.getElementById("question")
-var counter = document.getElementById("counter")
-var timeGauge = document.getElementById("timeGauge")
+var timer = document.getElementById("timer")
 var choiceA = document.getElementById("A")
 var choiceB = document.getElementById("B")
 var choiceC = document.getElementById("C")
 var choiceD = document.getElementById("D")
+var highscores = document.getElementById("highscores")
 
 //questions to be displayed
 
@@ -37,27 +37,19 @@ var questions = [
     },
     {
     question: "String values must be enclosed within _____",
-    choiceA: "1. Strings",
-    choiceB: "2. Booleans",
-    choiceC: "3. Alerts",
-    choiceD: "4. Numbers",
+    choiceA: "1. Commas",
+    choiceB: "2. Curly brackets",
+    choiceC: "3. Quotes",
+    choiceD: "4. Parenthesis",
     correct: "C"
     },
     {
     question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-    choiceA: "1. Strings",
-    choiceB: "2. Booleans",
-    choiceC: "3. Alerts",
-    choiceD: "4. Numbers",
-    correct: "C"
-    },
-    {
-    question: "String values must be enclosed within _____",
-    choiceA: "1. Strings",
-    choiceB: "2. Booleans",
-    choiceC: "3. Alerts",
-    choiceD: "4. Numbers",
-    correct: "C"
+    choiceA: "1. Javascript",
+    choiceB: "2. Terminal/bash",
+    choiceC: "3. For loops",
+    choiceD: "4. Console.log",
+    correct: "D"
     }
 ]
 console.log(questions[0])
@@ -71,9 +63,30 @@ function startQuiz(){
     //start timer
     showQuestions()
     quiz.style.display = "block"
+    countdown()
+
+
 }
 
+//timer
 
+var counter = 75
+function countdown() {
+    
+    setInterval(function() {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById('timer');
+        span.innerHTML = counter;
+     }
+     if (counter === 0) {
+        alert('sorry, out of time');
+        clearInterval(counter);
+      }
+    }, 1000);
+  };
+  
+  
 
 //runs the questions
 
@@ -88,21 +101,26 @@ function showQuestions() {
     choiceC.innerHTML = q.choiceC
     choiceD.innerHTML = q.choiceD
 }
+
 //checks what the user selected 
 
-/* function checkAnswer(answer){
+function checkAnswer(answer){
     if(questions[runningQuestionIndex].correct == answer){
-        
+        runningQuestionIndex++
+        showQuestions()
     }else{
-        //reduce time
+     counter = counter - 10
+     runningQuestionIndex++
+     showQuestions()
     }
 }
-if(runningQuestionIndex < lastQuestionIndex){
-    //runningQuestionIndex++
-    //showQuestions()
+if(runningQuestionIndex > lastQuestionIndex){
+    quiz.style.display = "none"
+    highscores.style.display = "block"
 }else{
     //show score
 }
-*/
 
 
+
+console.log(lastQuestionIndex)
